f3c699fa469ef0f30027934dca894c79d6911fc4
ematipico <estoppa@cloudflare.com>
2026-03-11 14:44:25 +0000

    more errors

--- build.mjs
+++ build.mjs
@@ -1,10 +1,9 @@
-import { build } from "vite";
+import { build } from 'vite';
 
 try {
-  await build();
-  console.log("Build succeeded");
+	await build();
+	console.log('\nBuild succeeded');
 } catch (e) {
-  console.table(e);
-  console.error("Build failed:", e.message);
-  process.exit(1);
+	console.error('Build failed:', e.message);
+	process.exit(1);
 }
